8dd66049087d62d44f977b3ae912606b68e42503
ruthadler <ruti.adr@gmail.com>
2026-01-19 16:23:16 +0700

    Add preprocessing placeholder

---
+++ src/preprocessing.py
@@ -0,0 +1,11 @@
+"""
+Data preprocessing and cleaning module for AirBnB rating prediction project.
+
+Handles:
+- Missing values
+- Outliers
+- Data type conversions
+- Feature cleaning
+"""
+
+# TODO: Add cleaning functions from notebook
