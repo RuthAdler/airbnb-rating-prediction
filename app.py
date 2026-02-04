f66bc095216a2c42f7e0bc2b7bab430fd8f6e2dd
Ido Friedman <idof25@gmail.com>
2026-02-04 17:17:23 +0200

    Add initial Streamlit app structure with model loading and prediction imports

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from predictor.load_model import load_model
+from predictor.pipeline import predict
+from predictor.config import DEFAULT_MODEL_PATH
+
+#TODO: Build a Streamlit app
